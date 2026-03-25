b149b891d1bb8e93eaab4066372d73139ecda4d8
alumno <artugonmo2007@gmail.com>
2026-03-25 09:28:56 +0100

    Subida archivos al repositorio v2

---
+++ src/Figuras/Paralelogramo.java
@@ -0,0 +1,53 @@
+package Figuras;
+
+public class Paralelogramo {
+    private double base;
+    private double altura;
+    private String tipo;
+
+    public Paralelogramo(double a, double b) {
+        base = a;
+        altura = b;
+    }
+
+    public void mostrar() {
+        tipo = "Rectángulo";
+        System.out.println("Tipo: " + tipo);
+        System.out.println("Ancho: " + base);
+        System.out.println("Alto: " + altura);
+
+        calcularPerimetro();
+        calcularArea();
+    }
+
+    private void calcularArea() {
+        double area = base * altura;
+        System.out.println("Área: " + area);
+    }
+
+    private void calcularPerimetro() {
+        System.out.println("Perímetro: " + ((base * 2) + (altura * 2)));
+    }
+
+    public boolean mismoTamanyo(boolean exacto, Paralelogramo paral) {
+        if (exacto) {
+            return this.base == paral.base && this.altura == paral.altura;
+        } else {
+            return Math.abs(this.base - paral.base) < 1 &&
+                    Math.abs(this.altura - paral.altura) < 1;
+        }
+    }
+
+    public double getBase() {
+        return base;
+    }
+    public void setBase(double base) {
+        this.base = base;
+    }
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+}
